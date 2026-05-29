fcdf14c5a6686a9f687301fde4bdf2e7950c6c9d
Fellipe Dultra <fellipesdultra10@gmail.com>
2026-05-29 19:25:46 -0300

    Adição de comentários

--- src/servico/ServicoNotificacoes.java
+++ src/servico/ServicoNotificacoes.java
@@ -11,14 +11,14 @@ public class ServicoNotificacoes {
         this.bancoDeNotificacoes = new ArrayList<>();
     }
 
-    // 3.2.1. Armazenar notificação enviada
+    // Armazenar notificação enviada
     public void armazenarNotificacaoEnviada(String tipo, String nomeUsuario, String mensagem) {
         Notificacao novaNotificacao = new Notificacao(tipo, nomeUsuario, mensagem);
         bancoDeNotificacoes.add(novaNotificacao);
         System.out.println("\nNotificação enviada com sucesso ao administrador!");
     }
 
-    // 3.2.2. Exibir Notificações Lidas
+    // Exibir Notificações Lidas
     public void exibirNotificacaoLida() {
         System.out.println("\n--- NOTIFICAÇÕES LIDAS ---");
         boolean encontrou = false;
@@ -33,7 +33,7 @@ public class ServicoNotificacoes {
         }
     }
 
-    // 3.2.3. Exibir Notificações Não Lidas (e já marca como lida após exibir)
+    // Exibir Notificações Não Lidas (e já marca como lida após exibir)
     public void exibirNotificacaoNaoLida() {
         System.out.println("\n--- NOTIFICAÇÕES NÃO LIDAS ---");
         boolean encontrou = false;
